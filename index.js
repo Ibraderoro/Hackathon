d7fdf8ca0f01a75e83ef4c3c0245aca69eeed8e5
Salisu Ibrahim <salisu.ibrahim15@yahoo.com>
2024-10-28 17:01:43 -0400

    update

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 let express = require('express');
 let app = express();
 let ytdl = require('ytdl-core');
-<<<<<<< HEAD
+
 let cors = require("cors")
 
-app.use(cors())
-=======
-const cors = require('cors');
->>>>>>> fbdbd19a3702872ab25c9216a7e8c0a192aba134
 
 app.use(cors());
 app.listen('4000', function(){
